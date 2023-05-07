1846192290c8c7087edff445a977c595ae145b41
kvaSSavk <71208972+KregTM@users.noreply.github.com>
2023-05-07 17:57:11 +0600

    kak po angliyski oruzhie (gun)

---
+++ app/src/main/java/com/kreg/mlkitcameraapp/cameraActivity/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.kreg.mlkitcameraapp.cameraActivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kreg.mlkitcameraapp.R
+
+class CameraActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
